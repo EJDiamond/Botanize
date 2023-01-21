120ae51fe05bb8e0fc52c471c331b03892d784b1
EJDiamond <emilydiamond321@gmail.com>
2023-01-21 18:27:36 +0000

    Add styling to anchors and icons in the app css to use throughout the project

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,11 +13,11 @@ const NavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link className={styles.NavLink}>
+                        <Nav.Link>
                             <i className="fa-solid fa-house-chimney"></i> Home</Nav.Link>
-                        <Nav.Link className={styles.NavLink}>
+                        <Nav.Link>
                             <i className="fas fa-sign-in-alt"></i> Sign in</Nav.Link>
-                        <Nav.Link className={styles.NavLink}>
+                        <Nav.Link>
                             <i className="fas fa-user-plus"></i> Sign up</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
