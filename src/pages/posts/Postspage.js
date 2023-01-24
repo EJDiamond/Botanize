f425efdf6ad6bc18624b5a8c5f14aa2a09d74c72
EJDiamond <emilydiamond321@gmail.com>
2023-01-24 13:19:34 +0000

    Add filter requests to API so the user can explore all posts, view their bookmarked posts and eventually view only posts from users they follow

--- src/pages/posts/Postspage.js
+++ src/pages/posts/Postspage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Container, Image, Row } from 'react-bootstrap'
+import { useLocation } from 'react-router-dom';
+import { axiosReq } from '../../api/axiosDefaults';
 
 import appStyles from "../../App.module.css";
+import NoResults from "../../assets/no-results.png";
+import Post from './Post';
+import Asset from '../../components/Asset';
 
-function Postspage() {
+function Postspage({ filter = "", message }) {
+    const [posts, setPosts] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const { data } = await axiosReq.get(`/posts/?${filter}`)
+                setPosts(data)
+                setHasLoaded(true)
+            } catch (err) {
+                console.log(err)
+            }
+        };
+        setHasLoaded(false)
+        fetchPosts();
+    }, [filter, pathname]);
+
     return (
         <Row className='h-100'>
             <Col className='d-none d-lg-block p-0 p-lg-2' lg={3}>
@@ -12,6 +35,23 @@ function Postspage() {
                 </Container>
             </Col>
             <Col className='py-2 p-0 p-lg-2' lg={6}>
+                {hasLoaded ? (
+                    <>
+                        {posts.results.length ? (
+                            posts.results.map((post) => (
+                                <Post key={post.id} {...post} setPosts={setPosts} />
+                            ))
+                        ) : (
+                            <Container className={appStyles.Content}>
+                                <Asset src={NoResults} message={message} />
+                            </Container>
+                        )}
+                    </>
+                ) : (
+                    <Container className={appStyles.Content}>
+                        <Asset spinner />
+                    </Container>
+                )}
                 <Container className={appStyles.Content}>
                     Answers
                 </Container>
