240c4b8ddbb093b3aa4664eb30734f32a397bb09
EJDiamond <emilydiamond321@gmail.com>
2023-01-25 14:33:37 +0000

    Add Context provider for the profile data so the it can be used in different parts of the app

--- src/pages/profiles/PlantWhisperers.js
+++ src/pages/profiles/PlantWhisperers.js
@@ -1,35 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Container } from 'react-bootstrap';
-import { axiosReq } from '../../api/axiosDefaults';
 import appStyles from "../../App.module.css";
 import Asset from '../../components/Asset';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { useProfileData } from '../../contexts/ProfileDataContext';
 import Profile from './Profile';
 
 const PlantWhisperers = ({ mobile }) => {
-  const [profileData, setProfileData] = useState({
-    pageProfile: { results: [] },
-    plantWhisperers: { results: [] },
-  });
-  const { plantWhisperers } = profileData;
-  const currentUser = useCurrentUser()
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          plantWhisperers: data,
-        }));
-      } catch (err) {
-        console.log(err)
-      }
-    };
-    handleMount()
-  }, [currentUser]);
+  const { plantWhisperers } = useProfileData();
 
   return (
     <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'}`}>
